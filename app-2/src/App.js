001c73af94195ffe5c661a6ae64c1c9b280d9e88
Hannah Shin <hannahshin@Hannahs-MBP.lan>
2021-04-21 23:18:43 -0700

    unit 3 day 3 hw done

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+  constructor (){
+    super();
+
+    this.state = {
+      foods: ['spaghetti', 'ice cream', 'sushi', 'bologna', 'cheese']
+    };
+  }
+
+
+  render() {
+    let foodsArray = this.state.foods.map((element, index) => {
+      return <h2 key ={index}>{element}</h2>
+    });
+
+    return <div className = 'App'>{foodsArray}</div>
+  }
 }
+
+
+
+// key = special string attribute need to include when creating list of elements and identify which items in the list are changed, updated, or deleted 
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Image from './Image.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render(){
+    return (
+      <div className = 'App'>
+        <Image url = { 'https://lh3.googleusercontent.com/proxy/VVHtywMXXM6wIOGbL9R7S8ySjvyDbevX2_2XnOv4zERLk_V3PVB4epbq6Gcv9jmdQNoTbwm-fibiGgIXta5wpKmr-mQWbKrRhfp7x9Q9TVaaH1wwqR8ygFC0ORbp5h6np5PfrQpdZqC8' } />
+      </div>
+    );
+  }
 }
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
